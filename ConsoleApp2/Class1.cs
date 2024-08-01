2a2e7d06d7ae6585ca24e7d708bd240e71822fcf
FATİH TALHA YAVUZASLANOĞLU <yfatihtalha@gmail.com>
2024-08-01 19:33:16 +0300

    bitt,,

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class1
+    {
+    }
+    public class CustomerManager
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("hello");
+        }
+        public void ShowAlert()
+        {
+            Console.WriteLine("Be carefull!");
+        }
+        public void SendMessage2(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public void ShowAlert2(string alert)
+        {
+            Console.WriteLine(alert);
+        }
+
+    }
+}
